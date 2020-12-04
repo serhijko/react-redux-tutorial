de989b9f2f843106bd564dfebcb60416be7bf6aa
serhijko <sp_by@tut.by>
2020-12-04 22:48:47 +0300

    Redux: Actions

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import store from './store';
+import { STORY_ARCHIVE } from './constants/actionTypes';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <App
         stories={store.getState().storyState}
-        onArchive={() => {}} />
+        onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })} />
   </React.StrictMode>,
   document.getElementById('root')
 );
