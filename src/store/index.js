23dfa1fe91bb83ba4d651c838625141dbc7bce66
serhijko <sp_by@tut.by>
2020-12-05 18:31:13 +0300

    Redux and Data Fetching

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,18 @@
 import { applyMiddleware, createStore } from 'redux';
-import { createLogger } from 'redux-logger/src';
+import { createLogger } from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
 import rootReducer from '../reducers';
+import rootSaga from '../sagas';
 
 const logger = createLogger();
+const saga = createSagaMiddleware();
 
 const store = createStore(
     rootReducer,
     undefined,
-    applyMiddleware(logger)
+    applyMiddleware(saga, logger)
 );
+
+saga.run(rootSaga);
 
 export default store;
