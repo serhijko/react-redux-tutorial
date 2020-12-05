d2cba5dc20124986b6e544b7edb05747f40fa46c
serhijko <sp_by@tut.by>
2020-12-05 07:36:42 +0300

    Redux: Selectors

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import store from './store';
+import { getReadableStories } from './selectors/story';
 import { STORY_ARCHIVE } from './constants/actionTypes';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <App
-        stories={store.getState().storyState}
+        stories={getReadableStories(store.getState())}
         onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })} />
   </React.StrictMode>,
   document.getElementById('root')
