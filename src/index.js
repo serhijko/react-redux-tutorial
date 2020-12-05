cee4ab4efd959b3a5956a2012f2698e593783028
serhijko <sp_by@tut.by>
2020-12-05 08:17:32 +0300

    Redux: Re-render React

--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ import { getReadableStories } from './selectors/story';
 import { STORY_ARCHIVE } from './constants/actionTypes';
 import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App
-        stories={getReadableStories(store.getState())}
-        onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })} />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function render() {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App
+                stories={getReadableStories(store.getState())}
+                onArchive={id => store.dispatch({type: STORY_ARCHIVE, id})}/>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
+
+store.subscribe(render);
+render();
