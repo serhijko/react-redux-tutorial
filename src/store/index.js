ac5fed391b609d01785dad07f1307900dd7dd912
serhijko <sp_by@tut.by>
2020-12-05 08:31:44 +0300

    Redux Middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,13 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import { createLogger } from 'redux-logger/src';
 import rootReducer from '../reducers';
 
+const logger = createLogger();
+
 const store = createStore(
-    rootReducer
+    rootReducer,
+    undefined,
+    applyMiddleware(logger)
 );
 
 export default store;
